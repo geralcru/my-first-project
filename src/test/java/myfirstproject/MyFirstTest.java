2bc5abe313532f3d437a43e2a9bd176c89caa8c5
Gerardo Alvarado <consalgra@yahoo.com>
2016-07-29 02:29:36 +0200

    updating

--- src/test/java/myfirstproject/MyFirstTest.java
+++ src/test/java/myfirstproject/MyFirstTest.java
@@ -2,6 +2,8 @@ package myfirstproject;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -13,4 +15,28 @@ public class MyFirstTest {
     public void should_return_the_same_string_when_uppercased() {
         assertThat("azerty".toUpperCase()).isEqualTo("AZERTY");
     }
+
+    @Test
+    public void should_verify_number (){
+         int i=3;
+        assertThat(i>7).isFalse();
+    }
+
+    @Test
+    public void should_return_random_number(){
+        Random r = new Random();
+        int valeurMin= 0;
+        int valeurMax= 20000;
+        int valeur = valeurMin + r.nextInt(valeurMax - valeurMin);
+
+    }
+
+    @Test
+    public void greet() {
+       String yourName ="Stef";
+        String name = "Gerardo";
+        String.format("Hi", name," my name is",yourName);
+    }
 }
+
+
