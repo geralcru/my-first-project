3521e8c390fe2dc4af8e9825889ba03ff4274e23
Gerardo Alvarado <consalgra@yahoo.com>
2016-09-11 11:10:12 +0200

    adding log message

--- src/test/java/Person.java
+++ src/test/java/Person.java
@@ -4,13 +4,14 @@ import org.junit.Test;
  * Created by gerardoalvarado on 29/07/2016.
  */
 public class Person {
-    String name = "step";
+    String name = "Step";
 
 
     @Test
     public void should_give_name(){
-        String yourName = "Gerardo";
+        String yourName = "ALVARADO";
         String.format("Hi %s, my name is %s", name, yourName);
+        System.out.println( name  + yourName);
 
     }
 }
